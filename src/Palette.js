3a967aedb5f1ca73e76b3a57338b5596bf600f11
Strilitz <asaf.strilitz@intel.com>
2020-03-07 14:36:05 +0200

    first commit

---
+++ src/Palette.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+class Palette extends Component {
+  render() {
+    return (
+      <div className="Palette">
+        {/* navbar goes here */}
+        <div className="palette-color"></div>
+        {/* footer */}
+      </div>
+    );
+  }
+}
+
+export default Palette;
