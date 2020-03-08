a12fa466fb2fbe90314c7b1e15df87fa1c42db7e
asafstr2 <asafstr2@gmail.com>
2020-03-08 19:03:07 +0200

    styles

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -6,19 +6,28 @@ export default function ColorBox(props) {
 
 	const { name, background } = props;
 	return (
-        
 		<div style={{ background }} className="ColorBox">
-			
-				<div className="box-container">
-					<span>{name}</span>
-				</div>
-				<CopyToClipboard text={background} onCopy={() => setCopied(true)}>
-					<button className="copy-button"> Copy</button>
-				</CopyToClipboard>
-                <div className="flexbox-fix">
+			<div className="box-container">
+				<span>{name}</span>
+			</div>
+			<CopyToClipboard
+				text={background}
+				onCopy={() => {
+					setCopied(true);
+					setTimeout(() => setCopied(false), 1500);
+				}}
+			>
+				<button className="copy-button"> Copy</button>
+			</CopyToClipboard>
+			<div className="flexbox-fix">
 				<span className="see-more">More</span>
-                <div  style={{ background }} className={`${!copied?"copy-overley":"copy-overley-true"}`}><span>Copied</span></div>
-                </div>
+
+				<div style={{ background }} className={`${!copied ? 'copy-overley' : 'copy-overley-true'}`} />
+			</div>
+			<div className={`${!copied ? 'copy-msg' : 'copy-msg-true'}`}>
+				<h1>Copied!</h1>
+				<p>{background}</p>
+			</div>
 		</div>
 	);
 }
