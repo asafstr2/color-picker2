7571d35ffd2d3e9b4391051c0d8dac2e8e1b1f67
Strilitz <asaf.strilitz@intel.com>
2020-03-09 01:35:22 +0200

    styled box

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -19,11 +19,10 @@ export default function ColorBox(props) {
 			>
 				<button className="copy-button"> Copy</button>
 			</CopyToClipboard>
-			<div className="flexbox-fix">
+			
 				<span className="see-more">More</span>
-
 				<div style={{ background }} className={`${!copied ? 'copy-overley' : 'copy-overley-true'}`} />
-			</div>
+		
 			<div className={`${!copied ? 'copy-msg' : 'copy-msg-true'}`}>
 				<h1>Copied!</h1>
 				<p>{background}</p>
