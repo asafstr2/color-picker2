e6124610455583595b3f9854b511555b0c54a2d2
asafstr2 <asafstr2@gmail.com>
2020-03-08 18:09:20 +0200

    flexbox fix

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import {CopyToClipboard} from 'react-copy-to-clipboard';
+import React, { useState } from 'react';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 import './ColorBox.css';
 export default function ColorBox(props) {
+	const [ copied, setCopied ] = useState(false);
+
 	const { name, background } = props;
 	return (
+        
 		<div style={{ background }} className="ColorBox">
-			<div className="box-container">
-				<span>{name}</span>
-			</div>
-            <CopyToClipboard text={background}>
-				<button className="copy-button"> Copy</button>
-			</CopyToClipboard>
-			<span className="see-more">More</span>
+			
+				<div className="box-container">
+					<span>{name}</span>
+				</div>
+				<CopyToClipboard text={background} onCopy={() => setCopied(true)}>
+					<button className="copy-button"> Copy</button>
+				</CopyToClipboard>
+                <div className="flexbox-fix">
+				<span className="see-more">More</span>
+                <div  style={{ background }} className={`${!copied?"copy-overley":"copy-overley-true"}`}><span>Copied</span></div>
+                </div>
 		</div>
 	);
 }
