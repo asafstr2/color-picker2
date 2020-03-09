5d5954a886bb5a8bc331caccb71e78146cc5e106
Strilitz <asaf.strilitz@intel.com>
2020-03-09 02:30:11 +0200

    chroma add

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 import Palette from "./Palette"
 import seedColor from "./seedColors"
 import {Route,Switch,Link,NavLink} from "react-router-dom"
+import { generatePalette } from "./ColorHelper";
 
 
 function App() {
+  		console.log(generatePalette(seedColor[4]));
+
   return (
 
 
