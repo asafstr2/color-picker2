2322bbc6befdf06e9c62eacda9741544aea53d04
Strilitz <asaf.strilitz@intel.com>
2020-03-09 06:31:53 +0200

    navbar start

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import Slider from "rc-slider";
+import "rc-slider/assets/index.css";
+
+import { Route, Switch, Link, NavLink } from "react-router-dom";
+
+import "./Navbar.css"
+class Navbar extends Component {
+render(){
+    const { level, changeLevel } = this.props;
+return (
+  <header className="Navbar">
+    <div className="logo">
+      <NavLink to="/"> reactcolorpicker</NavLink>
+    </div>
+    <div className="slider">
+      <Slider
+        defaultValue={level}
+        min={100}
+        max={900}
+        step={100}
+        onAfterChange={changeLevel}
+      />
+    </div>
+  </header>
+);
+}
+}
+export default Navbar;
--- src/Palette.js
+++ src/Palette.js
@@ -1,26 +1,26 @@
-import React, { Component } from 'react';
-import ColorBox from './ColorBox';
-import './palette.css';
-import Slider from "rc-slider";
-import "rc-slider/assets/index.css";
-
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import "./palette.css";
+import Navbar from "./Navbar"
 
 class Palette extends Component {
-state={
-	level:500,
-}
+  state = {
+    level: 500
+  };
 
-changeLevel=(level)=>{
-this.setState({level})
-}
+  changeLevel = level => {
+    this.setState({ level });
+  };
 
-	render() {
-		const { level } = this.state;
-		const { colors } = this.props.palette;
-		const colorBox =colors[level].map((color) => <ColorBox background={color.hex} name={color.name} />);
-		return (
+  render() {
+    const { level } = this.state;
+    const { colors } = this.props.palette;
+    const colorBox = colors[level].map(color => (
+      <ColorBox background={color.hex} name={color.name} />
+    ));
+    return (
       <div className="Palette">
-        <Slider defaultValue={level} min={100} max={900} step={100} onAfterChange={this.changeLevel}/>
+        <Navbar level={level} changeLevel={this.changeLevel}></Navbar>
         {/* navbar goes here */}
 
         <div className="palette-color">
@@ -29,7 +29,7 @@ this.setState({level})
         </div>
       </div>
     );
-	}
+  }
 }
 
 export default Palette;
