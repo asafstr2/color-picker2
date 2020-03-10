78f7e4cd328bb433ae7348fd3876877f0eeffa14
asafstr2 <asafstr2@gmail.com>
2020-03-10 11:22:16 +0200

    main route added

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@ import Palette from './Palette';
 import seedColor from './seedColors';
 import { Route, Switch, Link, NavLink } from 'react-router-dom';
 import { generatePalette } from './ColorHelper';
+import Palettelist from './Palettelist';
 
-function findPalette(id){
-  return seedColor.find(palette=>palette.id===id)
+function findPalette(id) {
+	return generatePalette(seedColor.find((palette) => palette.id === id));
 }
-
 
 function App() {
 	console.log(generatePalette(seedColor[4]));
 	return (
 		<div>
 			<Switch>
-				<Route exact path="/"  />
-        <Route exact path="/palette/:id"  
-        
-        render={routeProps=>(<Palette palette={generatePalette(findPalette(routeProps.match.params.id))} />)}
-        
-        />
+				<Route exact path="/" render={() => <Palettelist />} />
+				<Route
+					exact
+					path="/palette/:id"
+					render={(routeProps) => <Palette palette={findPalette(routeProps.match.params.id)} />}
+				/>
 			</Switch>
 		</div>
 	);
---
+++ src/Palettelist.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Palettelist extends Component {
+    render() {
+        return (
+            <div>
+                <h1>react color</h1>
+            </div>
+        )
+    }
+}
