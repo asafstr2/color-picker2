bcd5fd734f3567c614822b4837a21af8bb2003de
asafstr2 <asafstr2@gmail.com>
2020-03-10 09:54:44 +0200

    snack bar added

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import Slider from "rc-slider";
 import Select from "@material-ui/core/Select";
 import MenuItem from "@material-ui/core/MenuItem";
-import InputLabel from "@material-ui/core/InputLabel";
+import Snackbar from '@material-ui/core/Snackbar';
+
 
 import "rc-slider/assets/index.css";
 import { Route, Switch, Link, NavLink } from "react-router-dom";
@@ -11,18 +12,19 @@ import "./Navbar.css";
 class Navbar extends Component {
 
 state={
-  format:"hex"
+  format:"hex",
+  open:false
 }
 
-
   handleChange = e => {
-    this.setState({format:e.target.value})
+    this.setState({format:e.target.value,open:true})
     this.props.handleChange(e.target.value);
+    setTimeout(()=>this.setState({open:false}),1000)
   };
 
   render() {
     const { level, changeLevel } = this.props;
-    const { format } = this.state;
+    const { format,open ,handleClose} = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -42,10 +44,15 @@ state={
         <div className="select-container">
           <Select value={format} onChange={this.handleChange}>
            
-            <MenuItem value="hex">hex</MenuItem>
-            <MenuItem value="rgb">rgb</MenuItem>
-            <MenuItem value="rgba">rgba</MenuItem>
+            <MenuItem value="hex">HEX</MenuItem>
+            <MenuItem value="rgb">RGB</MenuItem>
+            <MenuItem value="rgba">RGBA</MenuItem>
           </Select>
+          <Snackbar open={open} anchorOrigin={ {vertical: 'bottom', horizontal: 'left'}} onClose={handleClose}
+      
+         message= {`DONE- ${format.toUpperCase()}`}
+    >
+      </Snackbar>
         </div>
       </header>
     );
